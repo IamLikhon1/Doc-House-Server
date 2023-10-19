b8570fa69764ee8b035649bff149032207e40a50
IamLikhon1 <likhonadnan55@gmail.com>
2023-10-18 20:52:23 -0700

    setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors= require("cors")
+const app = express();
+const port= process.env.PORT || 5000;
+require("dotenv").config
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Doc-House Is running')
+})
+app.listen(port,()=>{
+    console.log(`Doc-House is running on port: ${port}`)
+})
